1eb81eca4570a41ec7ef6b62d445789ad97c59d4
Daniel Feler <dfeler99@gmail.com>
2023-11-10 02:45:05 -0500

    Initial Commit

---
+++ Fetch Recipes/Fetch_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesApp.swift
+//  Fetch Recipes
+//
+//  Created by Daniel Feler on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
